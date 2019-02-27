930861116aa57a95220f0f0352b587ac3eb66ebc
Sebastian Palacio <sebastianpalacio327@gmail.com>
2019-02-26 23:33:07 -0500

    Insert data into HashTable

--- hashTable/hashTable.js
+++ hashTable/hashTable.js
@@ -14,12 +14,30 @@ function HashNode (key, value, next = null) {
 HashTable.prototype.hash = function (key) {
   let getCharCodes = char => char.charCodeAt(char)
   let sum = (acum, charCoded) => acum + charCoded
-  let total = key.split('')
-                 .map(getCharCodes)
-                 .reduce(sum, 0)
-
-  let bucket = total % this.numBuckets                 
-  return bucket
+  let total = key.split('').map(getCharCodes).reduce(sum, 0)
+  return total % this.numBuckets // Bucket
 } 
+
+HashTable.prototype.insert = function (key, value) {
+  let index = this.hash(key)
+  console.log('Index: ', index)
+  if (!this.buckets[index]) {
+    addNewBucket(key, value, index, this)
+  }  else {
+    addNodeToBucket(key, value, index, this)
+  }
+}
+
+function addNewBucket (key, value, index, HashTable) {
+  HashTable.buckets[index] = new HashNode(key, value)
+}
+
+function addNodeToBucket (key, value, index, HashTable) {
+  let currentNode = HashTable.buckets[index]
+  while (currentNode.next) {
+    currentNode = currentNode.next
+  }
+  currentNode.next = new HashNode(key, value)
+}
 
 module.exports = HashTable
--- hashTable/index.js
+++ hashTable/index.js
@@ -3,3 +3,9 @@ let HashTable = require('./hashTable')
 let myHashTable = new HashTable(30)
 
 console.log('Hash: ', myHashTable.hash('Becca'))
+
+myHashTable.insert('Dean', 'dean@gmail.com')
+myHashTable.insert('Megan', 'megan@gmail.com')
+myHashTable.insert('Dane', 'dane@gmail.com')
+
+console.log('myHashTable: ', myHashTable.buckets)
