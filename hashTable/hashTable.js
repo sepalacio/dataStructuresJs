be6e3ac1a38e60c7abedaec7e83627daeddcedce
Sebastian Palacio <sebastianpalacio327@gmail.com>
2019-02-27 23:32:48 -0500

    Update buckets

--- hashTable/hashTable.js
+++ hashTable/hashTable.js
@@ -16,20 +16,34 @@ HashTable.prototype.hash = function (key) {
   let sum = (acum, charCoded) => acum + charCoded
   let total = key.split('').map(getCharCodes).reduce(sum, 0)
   return total % this.numBuckets // Bucket
-} 
+}
 
 HashTable.prototype.insert = function (key, value) {
   let index = this.hash(key)
-  console.log('Index: ', index)
   if (!this.buckets[index]) {
     addNewBucket(key, value, index, this)
-  }  else {
+  } else {
     addNodeToBucket(key, value, index, this)
+  }
+}
+
+HashTable.prototype.update = function (key, value) {
+  let index = this.hash(key)
+  if (!this.buckets[index]) {
+    return false
+  } else if (this.buckets[index].key === key) {
+    updateBucket(this.buckets[index], value)
+  } else {
+    findNodeInAbucket(index, this, key, value)
   }
 }
 
 function addNewBucket (key, value, index, HashTable) {
   HashTable.buckets[index] = new HashNode(key, value)
+}
+
+function updateBucket (bucket, newValue) {
+  bucket.value = newValue
 }
 
 function addNodeToBucket (key, value, index, HashTable) {
@@ -38,6 +52,17 @@ function addNodeToBucket (key, value, index, HashTable) {
     currentNode = currentNode.next
   }
   currentNode.next = new HashNode(key, value)
+}
+
+function findNodeInAbucket (index, HashTable, key, value) {
+  let currentNode = HashTable.buckets[index]
+  while (currentNode.next) {
+    if (currentNode.next.key === key) {
+      updateBucket(currentNode.next, value)
+      return
+    }
+    currentNode = currentNode.next
+  }
 }
 
 module.exports = HashTable
--- hashTable/index.js
+++ hashTable/index.js
@@ -7,5 +7,7 @@ console.log('Hash: ', myHashTable.hash('Becca'))
 myHashTable.insert('Dean', 'dean@gmail.com')
 myHashTable.insert('Megan', 'megan@gmail.com')
 myHashTable.insert('Dane', 'dane@gmail.com')
-
+myHashTable.update('Dean', 'deanMachine@gmail.com')
+myHashTable.update('Megan', 'meganSmith@outlook.com')
+myHashTable.update('Dane', 'dane2Bdev@gmail.com')
 console.log('myHashTable: ', myHashTable.buckets)
